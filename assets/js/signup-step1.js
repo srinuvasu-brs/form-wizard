7b1b7b684a191c6a0580562cc634a1fa2107b36d
srinuvasu-brs <srinumsc@gmail.com>
2024-06-24 11:48:36 +0530

    update

--- assets/js/signup-step1.js
+++ assets/js/signup-step1.js
@@ -341,18 +341,18 @@ function validatePhoneNumber () {
   const PhoneInput = document.getElementById('phone');
   const phoneError = document.getElementById('phoneError');
   const phoneValue = PhoneInput.value;
+  if (!/^\*$/.test(phoneValue)) {
+    phoneError.textContent = 'Phone number must contain only digits.';
+    PhoneInput.value = phoneValue.replace(/\D/g, '');
+  } else {
+    phoneError.textContent = '';
+  }
   if (mobileNumber.length === 10 && /^\d+$/.test(mobileNumber)) {
     errorElement.textContent = ''; // Clear error message
     alert('Form submitted successfully with mobile number: ' + mobileNumber);
     // You can proceed with form submission or further processing
   } else {
     errorElement.textContent = 'Please enter exactly 10 digits';
-  }
-  if (!/^\*$/.test(phoneValue)) {
-    phoneError.textContent = 'Phone number must contain only digits.';
-    PhoneInput.value = phoneValue.replace(/\D/g, '');
-  } else {
-    phoneError.textContent = '';
   }
 }
 
