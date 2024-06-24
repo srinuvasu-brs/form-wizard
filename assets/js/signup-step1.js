eff135c2dc45e78647064b3c3c841c89f46c565a
srinuvasu-brs <srinumsc@gmail.com>
2024-06-24 11:40:32 +0530

    update

--- assets/js/signup-step1.js
+++ assets/js/signup-step1.js
@@ -337,32 +337,17 @@ function toggle() {
   }
 }
 
-function validateForm(event) {
-  event.preventDefault();
-  
-  var mobileNumber = document.getElementById('mobileNumber').value;
-  var errorElement = document.getElementById('error-msg');
-  
-  if (/^\d{10}$/.test(mobileNumber)) {
-    errorElement.textContent = ''; // Clear error message
-    alert('Form submitted successfully!');
-    // You can proceed with form submission or further processing
+function validatePhoneNumber () {
+  const PhoneInput = document.getElementById('phone');
+  const phoneError = document.getElementById('phoneError');
+  const phoneValue = PhoneInput.value;
+  if (!/^\*$/.test(phoneValue)) {
+    phoneError.textContent = 'Phone number must contain only digits.';
+    PhoneInput.value = phoneValue.replace(/\D/g, '');
   } else {
-    errorElement.textContent = 'Please enter a 10-digit mobile number';
+    phoneError.textContent = '';
   }
 }
-
-// function validatePhoneNumber () {
-//   const PhoneInput = document.getElementById('phone');
-//   const phoneError = document.getElementById('phoneError');
-//   const phoneValue = PhoneInput.value;
-//   if (!/^\*$/.test(phoneValue)) {
-//     phoneError.textContent = 'Phone number must contain only digits.';
-//     PhoneInput.value = phoneValue.replace(/\D/g, '');
-//   } else {
-//     phoneError.textContent = '';
-//   }
-// }
 
 // function updateCharCount() {
 //   const textarea = documen.getElementById('type');
